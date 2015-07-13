f4fec933d092b1939906798f76b7ed279b35619b
fjbelchi <fjbelchi@gmail.com>
2015-07-14 00:46:49 +0200

    => operator tests added

--- DecodableTests/DecodableOperatorsTests.swift
+++ DecodableTests/DecodableOperatorsTests.swift
@@ -24,6 +24,49 @@ class DecodableOperatorsTests: XCTestCase {
         XCTAssertEqual(string, value)
     }
     
+    func testDecodeAnyDecodableDictionarySuccess() {
+        // given
+        let key = "key"
+        let value: NSDictionary = [key : "value"]
+        let dictionary: NSDictionary = [key: value]
+        // when
+        let result = try! dictionary => key
+        // then
+        XCTAssertEqual(result, value)
+    }
+    
+    func testDecodeAnyDecodableOptionalSuccess() {
+        // given
+        let key = "key"
+        let value = "value"
+        let dictionary: NSDictionary = [key: value]
+        // when
+        let string = try! dictionary => key as String?
+        // then
+        XCTAssertEqual(string!, value)
+    }
+    
+    func testDecodeAnyDecodableOptionalNilSuccess() {
+        // given
+        let key = "key"
+        let dictionary: NSDictionary = [key: NSNull()]
+        // when
+        let string = try! dictionary => key as String?
+        // then
+        XCTAssertNil(string)
+    }
+    
+    func testDecodeAnyDecodableArraySuccess() {
+        // given
+        let key = "key"
+        let value: NSArray = ["value1", "value2", "value3"]
+        let dictionary: NSDictionary = [key: value]
+        // when
+        let result = try! dictionary => key as Array<String>
+        // then
+        XCTAssertEqual(result, value)
+    }
+    
     // MARK: => Errors
     
     func testDecodeAnyDecodableThrowMissingKeyException() {
@@ -50,6 +93,52 @@ class DecodableOperatorsTests: XCTestCase {
         do {
             try noDictionary => key as String
         } catch DecodingError.JSONNotObject {
+            // then
+            XCTAssertTrue(true)
+        } catch {
+            XCTFail("should not throw this exception")
+        }
+    }
+    
+    func testDecodeAnyDecodableDictionaryThrowMissingKeyException() {
+        // given
+        let key = "key"
+        let value: NSDictionary = [key : "value"]
+        let dictionary: NSDictionary = [key: value]
+        // when
+        do {
+            try dictionary => "nokey"
+        } catch DecodingError.MissingKey(let key, _) {
+            // then
+            XCTAssertEqual(key, "nokey")
+        } catch {
+            XCTFail("should not throw this exception")
+        }
+    }
+    
+    func testDecodeAnyDecodableDictionaryThrowJSONNotObjectException() {
+        // given
+        let key = "key"
+        let noDictionary: NSString = "noDictionary"
+        // when
+        do {
+            try noDictionary => key
+        } catch DecodingError.JSONNotObject {
+            // then
+            XCTAssertTrue(true)
+        } catch {
+            XCTFail("should not throw this exception")
+        }
+    }
+    
+    func testDecodeAnyDecodableDictionaryThrowTypeMismatchException() {
+        // given
+        let key = "key"
+        let dictionary: NSDictionary = [key: "value"]
+        // when
+        do {
+            try dictionary => key
+        } catch DecodingError.TypeMismatch {
             // then
             XCTAssertTrue(true)
         } catch {
