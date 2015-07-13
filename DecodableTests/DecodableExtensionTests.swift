a7642075b33a9947a073dc697cfafdce0d5e1983
fjbelchi <fjbelchi@gmail.com>
2015-07-13 23:52:27 +0200

    Added Extension tests

--- DecodableTests/DecodableExtensionTests.swift
+++ DecodableTests/DecodableExtensionTests.swift
@@ -2,8 +2,84 @@
 //  DecodableExtensionTests.swift
 //  Decodable
 //
-//  Created by Fran_DEV on 13/07/15.
+//  Created by FJBelchi on 13/07/15.
 //  Copyright © 2015 anviking. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import Decodable
+
+class DecodableExtensionTests: XCTestCase {
+    
+    // MARK: String
+    func testStringDecodableSuccess() {
+        //given
+        let anyObject = "hello"
+        //when
+        let string = try! String.decode(anyObject)
+        //then
+        XCTAssertEqual(string, anyObject)
+    }
+    
+    func testStringDecodableFail() {
+        //given
+        let anyObject = 0
+        //when
+        do {
+            try String.decode(anyObject)
+        } catch DecodingError.TypeMismatch {
+            //then
+            XCTAssertTrue(true)
+        } catch {
+            XCTFail("should not throw this exception")
+        }
+    }
+    
+    // MARK: Int
+    func testIntDecodable() {
+        //given
+        let anyObject = 0
+        //when
+        let int = try! Int.decode(anyObject)
+        //then
+        XCTAssertEqual(int, anyObject)
+    }
+    
+    func testIntDecodableFail() {
+        //given
+        let anyObject = ""
+        //when
+        do {
+            try Int.decode(anyObject)
+        } catch DecodingError.TypeMismatch {
+            //then
+            XCTAssertTrue(true)
+        } catch {
+            XCTFail("should not throw this exception")
+        }
+    }
+    
+    // MARK: Double
+    func testDoubleDecodable() {
+        //given
+        let anyObject = 0.5
+        //when
+        let double = try! Double.decode(anyObject)
+        //then
+        XCTAssertEqual(double, anyObject)
+    }
+    
+    func testDoubleDecodableFail() {
+        //given
+        let anyObject = ""
+        //when
+        do {
+            try Double.decode(anyObject)
+        } catch DecodingError.TypeMismatch {
+            //then
+            XCTAssertTrue(true)
+        } catch {
+            XCTFail("should not throw this exception")
+        }
+    }
+}
