232b8c5c09d209dbf5ee5a16625bf57d4bd08d13
Johannes Lund <anviking@me.com>
2015-10-03 13:16:53 +0200

    Create Castable file

---
+++ Decodable/Castable.swift
@@ -0,0 +1,28 @@
+//
+//  Castable.swift
+//  Decodable
+//
+//  Created by Johannes Lund on 2015-09-25.
+//  Copyright © 2015 anviking. All rights reserved.
+//
+
+import Foundation
+
+public protocol Castable: Decodable {}
+
+extension Castable {
+    public static func decode(j: AnyObject) throws -> Self {
+        guard let result = j as? Self else {
+            let info = DecodingError.Info(object: j)
+            throw DecodingError.TypeMismatch(type: j.dynamicType, expectedType: self, info: info)
+            
+        }
+        return result
+    }
+}
+
+extension String: Castable {}
+extension Int: Castable {}
+extension Double: Castable {}
+extension Bool: Castable {}
+extension Dictionary: Castable {}
--- Decodable/Decodable.swift
+++ Decodable/Decodable.swift
@@ -12,25 +12,6 @@ public protocol Decodable {
     static func decode(json: AnyObject) throws -> Self
 }
 
-public protocol Castable: Decodable {}
-
-extension Castable {
-    public static func decode(j: AnyObject) throws -> Self {
-        guard let result = j as? Self else {
-            let info = DecodingError.Info(object: j)
-            throw DecodingError.TypeMismatch(type: j.dynamicType, expectedType: self, info: info)
-
-        }
-        return result
-    }
-}
-
-extension String: Castable {}
-extension Int: Castable {}
-extension Double: Castable {}
-extension Bool: Castable {}
-extension Dictionary: Castable {}
-
 extension NSDictionary {
     public static func decode(j: AnyObject) throws -> NSDictionary {
         guard let result = j as? NSDictionary else {
