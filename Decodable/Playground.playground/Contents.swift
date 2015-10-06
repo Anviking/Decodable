1dcd45330f97b9c6ea0597b4a467961383568fc1
Johannes Lund <anviking@me.com>
2015-10-06 21:41:36 +0200

    Assume unchanged notice in playground

--- Decodable/Playground.playground/Contents.swift
+++ Decodable/Playground.playground/Contents.swift
@@ -1,4 +1,7 @@
 //: Playground - noun: a place where people can play
 
+// If you want to ignore changes in this playground:
+// git update-index --assume-unchanged Decodable/Playground.playground/Contents.swift
+
 import UIKit
 import Decodable
