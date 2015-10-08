1ccff7eb767809700dc3e767f918eccf67a44b47
Johannes Lund <anviking@me.com>
2015-10-08 20:58:04 +0200

    Explain new decodeArray

--- Decodable/DecodeArray.swift
+++ Decodable/DecodeArray.swift
@@ -8,8 +8,7 @@
 
 import Foundation
 
-// Note: Favor use of [T].decode(json) declared in Decodable.swift
-
+/// Designed to be used with parse(json, path, decodeClosure) as the decodeClosure. Thats why it's curried and a "top-level" function instead of a function in an array extension. For everyday use, prefer using [T].decode(json) instead (declared in Decodable.swift).
 public func decodeArray<T>(elementDecodeClosure: AnyObject throws -> T)(json: AnyObject) throws -> [T] {
     
     guard let array = json as? [AnyObject] else {
