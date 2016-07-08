806867816d2dd7303fcfe8e66028b7dc039bd702
Johannes Lund <anviking@me.com>
2016-07-09 01:19:48 +0200

    Add KeyPath struct

---
+++ Sources/KeyPath.swift
@@ -0,0 +1,33 @@
+//
+//  KeyPath.swift
+//  Decodable
+//
+//  Created by Johannes Lund on 2016-07-09.
+//  Copyright © 2016 anviking. All rights reserved.
+//
+
+import Foundation
+
+public struct KeyPath: StringLiteralConvertible, ArrayLiteralConvertible {
+    var keys: [String]
+    
+    public init(keys: [String]) {
+        self.keys = keys
+    }
+    
+    public init(stringLiteral value: String) {
+        self.keys = [value]
+    }
+    
+    public init(extendedGraphemeClusterLiteral value: String) {
+        self.keys = [value]
+    }
+    
+    public init(unicodeScalarLiteral value: String) {
+        self.keys = [value]
+    }
+    
+    public init(arrayLiteral elements: String...) {
+        self.keys = elements
+    }
+}
