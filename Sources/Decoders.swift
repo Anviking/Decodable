a67bc010179170df78bcad18bec5250b5fe043f3
Johannes Lund <anviking@me.com>
2016-07-29 09:53:46 +0200

    Rename decoder parameters

--- Sources/Decoders.swift
+++ Sources/Decoders.swift
@@ -14,14 +14,14 @@ extension Optional {
     ///
     /// This function is used by `=>` and `=>?` overloads when decoding `T?`
     ///
-    /// - parameter decodeClosure: A decoder (decode closure) for the wrapped type
+    /// - parameter wrappedDecoder: A decoder (decode closure) for the wrapped type
     /// - returns: A closure takes an JSON object, checks it's `NSNull`, if so returns `nil`, otherwise calls the wrapped decode closure.
-    static func decoder(_ decodeClosure: (AnyObject) throws -> Wrapped) -> (AnyObject) throws -> Wrapped? {
+    static func decoder(_ wrappedDecoder: (AnyObject) throws -> Wrapped) -> (AnyObject) throws -> Wrapped? {
         return { json in
             if json is NSNull {
                 return nil
             } else {
-                return try decodeClosure(json)
+                return try wrappedDecoder(json)
             }
         }
     }
@@ -33,12 +33,12 @@ extension Array {
     ///
     /// This function is used by `=>` and `=>?` overloads when decoding `[T]`
     ///
-    /// - parameter decodeClosure: A decoder (decode closure) for the `Element` type
+    /// - parameter elementDecoder: A decoder (decode closure) for the `Element` type
     /// - throws: if `NSArray.decode` throws or any element decode closure throws
     /// - returns: A closure that takes an `NSArray` and maps it using the element decode closure
-    public static func decoder(_ elementDecodeClosure: (AnyObject) throws -> Element) -> (AnyObject) throws -> Array<Element> {
+    public static func decoder(_ elementDecoder: (AnyObject) throws -> Element) -> (AnyObject) throws -> Array<Element> {
         return { json in
-            return try NSArray.decode(json).map { try elementDecodeClosure($0) }
+            return try NSArray.decode(json).map { try elementDecoder($0) }
         }
     }
 }
@@ -51,11 +51,11 @@ extension Dictionary {
     /// - parameter key: A decoder (decode closure) for the `Key` type
     /// - parameter value: A decoder (decode closure) for the `Value` type
     /// - returns: A closure that takes a `NSDictionary` and "maps" it using key and value decode closures
-    public static func decoder(key keyDecodeClosure: (AnyObject) throws -> Key, value elementDecodeClosure: (AnyObject) throws -> Value) -> (AnyObject) throws -> Dictionary {
+    public static func decoder(key keyDecoder: (AnyObject) throws -> Key, value valueDecoder: (AnyObject) throws -> Value) -> (AnyObject) throws -> Dictionary {
         return { json in
             var dict = Dictionary()
             for (key, value) in try NSDictionary.decode(json) {
-                try dict[keyDecodeClosure(key)] = elementDecodeClosure(value)
+                try dict[keyDecoder(key)] = valueDecoder(value)
             }
             return dict
         }
