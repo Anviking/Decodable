dbb7838c5c78754a8b9d648fe117a37bc14c8350
Johannes Lund <anviking@me.com>
2017-10-29 03:32:59 +0100

    add new overloads

--- Sources/Operators.swift
+++ Sources/Operators.swift
@@ -17,3 +17,36 @@ precedencegroup DecodingPrecedence {
 
 infix operator =>  : DecodingPrecedence
 infix operator =>? : DecodingPrecedence
+
+// Dict -> T
+func => <A: Decodable, Key>(container: KeyedDecodingContainer<Key>, key: Key) throws -> A {
+    return try container.decode(A.self, forKey: key)
+}
+
+// Dict -> Dict
+func => <Key>(container: KeyedDecodingContainer<Key>, key: Key) throws -> KeyedDecodingContainer<Key> {
+    return try container.nestedContainer(keyedBy: Key.self, forKey: key)
+}
+
+// Dict -> Dict?
+func => <Key>(container: KeyedDecodingContainer<Key>, key: Key) throws -> KeyedDecodingContainer<Key>? {
+    if container.contains(key) {
+        return try container.nestedContainer(keyedBy: Key.self, forKey: key)
+    } else {
+        return nil
+    }
+}
+
+// Dict? -> Dict?
+func =>? <Key>(container: KeyedDecodingContainer<Key>?, key: Key) throws -> KeyedDecodingContainer<Key>? {
+    if let container = container, container.contains(key) {
+        return try container.nestedContainer(keyedBy: Key.self, forKey: key)
+    } else {
+        return nil
+    }
+}
+
+// Dict? -> Dict
+func => <Key>(container: KeyedDecodingContainer<Key>?, key: Key) throws -> KeyedDecodingContainer<Key>? {
+    return try container?.nestedContainer(keyedBy: Key.self, forKey: key)
+}
