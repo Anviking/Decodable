6ec043ebf3bca5d94c7032fcef776c4aa8691e71
Johannes Lund <anviking@me.com>
2017-11-22 18:51:22 +0100

    remove code generation

--- Generator/Templates/Documentation.swift
+++
@@ -1,7 +0,0 @@
-/// Retrieves the object at `path` from `json` and decodes it according to the return type
-/// 
-/// - parameter json: An object from NSJSONSerialization, preferably a `NSDictionary`.
-/// - parameter path: {path}
-/// - throws: {throws}
-/// - returns: {returns}
-///
