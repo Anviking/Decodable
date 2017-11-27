7acae83307bac15d963aa21ecaa865d4e3b1c901
Johannes Lund <anviking@me.com>
2017-11-27 10:08:29 +0100

    cleaning

--- Sources/Decodable.swift
+++ Sources/Decodable.swift
@@ -12,7 +12,6 @@ public protocol Decodable {
     static func decode(_ json: JSON) throws -> Self
 }
 
-
 extension Dictionary: Decodable where Key: Decodable, Value: Decodable {
     public static func decode(_ j: JSON) throws -> Dictionary {
         return try Dictionary.decoder(key: Key.decode, value: Value.decode)(j)
@@ -30,18 +29,6 @@ extension Optional: Decodable where Wrapped: Decodable {
         return try Optional.decoder(Wrapped.decode)(j)
     }
 }
-
-/* FIXME: this causes ambiguity issues, in the meantime resort to `Dictionary.decoder`
- https://github.com/Anviking/Decodable/issues/120
- 
-extension Dictionary where Key: Decodable, Value: Any {
-    
-    public static func decode(_ j: JSON) throws -> Dictionary {
-        let valueDecoder: (Any) throws -> Value = { try cast($0) }
-        return try Dictionary.decoder(key: Key.decode, value: valueDecoder)(j)
-    }
-}
-*/
 
 extension Array where Element: Decodable {
     public static func decode(_ j: JSON, ignoreInvalidObjects: Bool = false) throws -> [Element] {
